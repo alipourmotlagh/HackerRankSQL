b0d7c615490ca847da0299e934083b910da3377f
mohammad hadi alipour motlagh <alipourmotlagh@gmail.com>
2022-12-01 13:53:27 +0100

    Update Exercises.sql

--- Exercises.sql
+++ Exercises.sql
@@ -139,3 +139,33 @@ select n,case when n = (select n
         else 'Leaf' end as NodeType
 from bst
 order by n;
+
+
+/*
+Amber's conglomerate corporation just acquired some new companies. 
+Each of the companies follows this hierarchy: 
+
+Given the table schemas below, write a query to print the company_code, 
+founder name, total number of lead managers, 
+total number of senior managers, total number of managers, 
+and total number of employees. Order your output by ascending company_code.
+
+Note:
+
+The tables may contain duplicate records.
+The company_code is string, so the sorting should not be numeric. For example, if the company_codes are C_1, C_2, and C_10, then the ascending company_codes will be C_1, C_10, and C_2.
+*/
+
+select c.company_code,c.founder, count(distinct(lm.lead_manager_code)),count(distinct(sm.senior_manager_code)),count(distinct(m.manager_code)),
+count(distinct(e.employee_code))
+from company as c
+left join lead_manager as lm
+on c.company_code=lm.company_code
+left join senior_manager as sm
+on lm.lead_manager_code=sm.lead_manager_code
+left join manager as m
+on sm.senior_manager_code=m.senior_manager_code
+left join employee e
+on m.manager_code=e.manager_code
+group by c.founder, c.company_code
+order by c.company_code
